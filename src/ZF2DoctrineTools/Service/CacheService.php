210ec95ca0c0b8b043d690f092af4edc8343c14d
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2013-09-25 11:01:00 +0300

    Fix dependency loop

--- src/ZF2DoctrineTools/Service/CacheService.php
+++ src/ZF2DoctrineTools/Service/CacheService.php
@@ -5,7 +5,7 @@ namespace ZF2DoctrineTools\Service;
  * @author Laurynas Tretjakovas(n3ziniuka5) <laurynas.tretjakovas@gmail.com>
  * @property array    $cacheArray
  */
-class CacheService extends AbstractService
+class CacheService
 {
 
     protected $cacheArray = array();
